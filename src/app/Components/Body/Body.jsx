ddd2f4df5f9ea2a108df06b3410e44d9b314042e
shehryar-munir <shehryar.munir@dubizzlelabs.com>
2024-10-29 00:38:56 +0500

    Made response design for both header and body

--- src/app/Components/Body/Body.jsx
+++ src/app/Components/Body/Body.jsx
@@ -4,15 +4,15 @@ import './Body.css'
 export default function Body(props) {
     return (
 
-        <div className="flex flex-row justify-between items-center">
-            <div className="flex flex-col">
+        <div className="body-container">
+            <div>
                 <h1 className={"text-9xl"}>Hello</h1>
-                <div className="flex flex-row items-center">
+                <div className="intro-container">
                     <p className="mb-4 mx-2">__</p>
                     <p>Its Shehryar Munir, a Software Engineer</p>
                 </div>
             </div>
-            <div className={"flex flex-col mx-4 animate text-2xl"}> {/* Add the animate class here */}
+            <div className={"flex flex-col mx-4 animate text-2xl in-progress-container"}> {/* Add the animate class here */}
                 Portfolio in progress....
             </div>
         </div>
